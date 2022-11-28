5c44c3e02023279985af6b59de9076240ea69599
Jen-ie <80427517+Jen-ie@users.noreply.github.com>
2022-11-28 14:06:17 +0000

    First Commit

---
+++ 1. Setup.ps1
@@ -0,0 +1,10 @@
+﻿#Check existing version
+$PSVersiontable
+#Set an execution policy
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+#install MuGet as a package provider
+Install-PackageProvider Nuget -MinimumVersion 2.8.5.201 -Force | Out-Null
+#Install the module
+Install-Module -Name PowerShellGet -Force -AllowClobber
+#make directory for Poweshell
+mkdir c:\PowerShell
